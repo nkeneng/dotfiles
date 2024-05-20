0f64c64085da52501beac0a07fd29e4cca92bd30
stevennkeneng <nkeneng.junior@yahoo.fr>
2024-05-20 23:21:25 +0200

    added macos custom conf

---
+++ macos/.custom.zsh
@@ -0,0 +1,30 @@
+
+# Zoxide setup
+eval "$(zoxide init zsh)"
+eval "$(fzf --zsh)"
+
+alias cdl='zoxide query -l -s | less'
+alias cd=z
+
+if [[ ! "$PATH" == *$HOME/.local/bin* ]]; then
+	PATH="${PATH:+${PATH}:}$HOME/.local/bin"
+fi
+
+alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+
+alias para="prlctl"
+alias ubuntu="ssh ubuntu"
+
+export OLLAMA_ORIGINS="app://obsidian.md*,chrome-extension://*"
+
+export PATH=$PATH:/Users/stevennkeneng/Library/Python/3.9/bin
+alias wm="/Users/stevennkeneng/Documents/Website/42-Berlin/libft-war-machine/grademe.sh"
+
+
+function wm_libft() {
+    wm $(find . -type f -name '*.c' | sed 's/\.c$//')
+}
+
+export BAT_THEME=TwoDark
+
+alias norminette="find . -type f -name '*.h' -o -name '*.c' ! -name 'main.c' -exec norminette {} +"
