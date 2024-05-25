a911b01e1e96c340ae1c92a923504a9ebbb7c990
stevennkeneng <nkeneng.junior@yahoo.fr>
2024-05-25 20:59:22 +0200

    added custom keymap to accept copilot suggestion

--- config/.config/nvim/lua/plugins/copilot.lua
+++ config/.config/nvim/lua/plugins/copilot.lua
@@ -3,6 +3,11 @@ return {
     "github/copilot.vim",
     config = function()
       vim.cmd("Copilot enable")
+ 	  vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+          expr = true,
+          replace_keycodes = false
+        })
+        vim.g.copilot_no_tab_map = true
     end,
   },
 }
