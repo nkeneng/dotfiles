8c020c63d70b3f5dae6b5bcec7794f45e2b37587
42 env root <nkeneng.junior+42env@gmail.com>
2024-06-04 14:06:02 +0000

    added 42 env conf

---
+++ 42-env/.custom.zsh
@@ -0,0 +1,13 @@
+
+alias cc="cc -Wall -Wextra -Werror -g"
+alias tmux="tmux -2"
+
+export USER="snkeneng"
+export MAIL="snkeneng@student.42berlin.de"
+
+export LC_ALL=en_IN.UTF-8
+export LANG=en_IN.UTF-8
+
+alias francinette=/root/francinette/tester.sh
+
+alias paco=/root/francinette/tester.sh
