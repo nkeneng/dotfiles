4788a50522e2f3d26047b2b1825f88b83be5c096
42 env root <nkeneng.junior+42env@gmail.com>
2024-06-04 14:51:26 +0000

    added copilot chat and some 42 env config

--- config/.config/nvim/lua/plugins/copilot.lua
+++ config/.config/nvim/lua/plugins/copilot.lua
@@ -10,5 +10,18 @@ return {
         vim.g.copilot_no_tab_map = true
     end,
   },
+	{
+	"CopilotC-Nvim/CopilotChat.nvim",
+	branch = "canary",
+	dependencies = {
+	{ "github/copilot.vim" }, -- or github/copilot.vim
+	{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+	},
+	opts = {
+	debug = true, -- Enable debugging
+	-- See Configuration section for rest
+	},
+	-- See Commands section for default commands if you want to lazy load on them
+	},
 }
 
