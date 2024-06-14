16e4e226d6e548f21b98246e812b1192913911fb
stevennkeneng <nkeneng.junior@yahoo.fr>
2024-06-14 12:02:16 +0200

    added conditionning on includes.zsh

--- misc/.includes.zsh
+++ misc/.includes.zsh
@@ -1,3 +1,11 @@
-source ~/.alias.zsh
-source ~/.fzf.zsh
-source ~/.custom.zsh
+if [ -f ~/.alias.zsh ]; then
+  source ~/.alias.zsh
+fi
+
+if [ -f ~/.fzf.zsh ]; then
+  source ~/.fzf.zsh
+fi
+
+if [ -f ~/.custom.zsh ]; then
+  source ~/.custom.zsh
+fi
