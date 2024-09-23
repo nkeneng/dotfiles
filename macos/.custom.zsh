4a0c6e1af59ca46f1a48c7ffb19a3e51e0ace2f4
stevennkeneng <nkeneng.junior@yahoo.fr>
2024-09-23 06:41:25 +0200

    added tmux resurrect to main config

--- macos/.custom.zsh
+++ macos/.custom.zsh
@@ -27,7 +27,7 @@ function wm_libft() {
 
 export BAT_THEME=TwoDark
 
-alias norminette="find . -type f -name '*.h' -o -name '*.c' ! -name 'main.c' -exec norminette {} +"
+alias norminette="find . -type f -name '*.h' -o -name '*.c' ! -name 'main.c' ! -name 'test.c' -exec norminette {} +"
 
 alias port-forward="~/Documents/Bash_Scripts/port-forward.sh"
 alias port-delete="~/Documents/Bash_Scripts/del-port-forward.sh"
@@ -37,8 +37,46 @@ eval "$(gh copilot alias -- zsh)"
 
 source ~/.iterm2_shell_integration.zsh
 
-export USER="snkeneng"
+# export USER="snkeneng"
+export USER="stevennkeneng"
 export MAIL="snkeneng@student.42berlin.de"
 
 export EDITOR=nvim
 alias obs="cd '/Users/stevennkeneng/Library/Mobile Documents/iCloud~md~obsidian/Documents/Coding journey' && nvim ."
+
+
+# nvim config switcher
+alias nvim-lazy="NVIM_APPNAME=lazy_nvim nvim"
+
+function nvims() {
+  items=("default" "lazy_nvim")
+  config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+  if [[ -z $config ]]; then
+    echo "Nothing selected"
+    return 0
+  elif [[ $config == "default" ]]; then
+    config=""
+  fi
+  NVIM_APPNAME=$config nvim $@
+}
+
+# bindkey -s ^a "nvims\n"
+
+alias bashly='docker run --rm -it --user $(id -u):$(id -g) --volume "$PWD:/app" dannyben/bashly'
+
+export ANDROID_HOME=/Users/stevennkeneng/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+42-code() {
+	docker stop 42
+	docker rm 42
+	docker run -it \
+		--name 42 \
+		-e PUID=1002 \
+		-e PGID=1002 \
+		-v ${PWD}:/home/coder/code \
+		-v ~/.ssh:/home/coder/.ssh \
+		registry.stevenshomelab.com/42-env
+}
+
