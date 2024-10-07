80ce6a4d0358bba216090546dfb98f1ce089a94a
stevennkeneng <nkeneng.junior@yahoo.fr>
2024-10-07 21:52:55 +0200

    changed wezterm colorscheme to tokyonight

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,20 +4,22 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.color_scheme = 'Tokyo Night'
+
 -- This is where you actually apply your config choices
 
 -- my coolnight colorscheme
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#000000",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-}
+-- config.colors = {
+-- 	-- foreground = "#CBE0F0",
+-- 	-- background = "#000000",
+-- 	-- cursor_bg = "#47FF9C",
+-- 	-- cursor_border = "#47FF9C",
+-- 	-- cursor_fg = "#011423",
+-- 	-- selection_bg = "#033259",
+-- 	-- selection_fg = "#CBE0F0",
+-- 	-- ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+-- 	-- brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+-- }
 
 
 config.keys = {
