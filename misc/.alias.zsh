5f83d92df2705733b9e62687f15f2dd9adb0118b
stevennkeneng <nkeneng.junior@yahoo.fr>
2024-10-07 21:53:41 +0200

    changed eza aliasing back to previous version

--- misc/.alias.zsh
+++ misc/.alias.zsh
@@ -1,7 +1,7 @@
 alias lz=lazygit
 alias dockerps="docker ps --format '------------------------------------------------\nID: {{.ID}}\nNames: {{.Names}}\nImage: {{.Image}}\nCommand: {{.Command}}\nCreatedAt: {{.CreatedAt}}\nStatus: {{.Status}}\nPorts: {{.Ports}}\nSize: {{.Size}}' | awk -v RS='' -v ORS='\n' '1'"
 # check if eza is installed
-#if command -v eza &> /dev/null; then
-#	alias ls="eza --color=always --long --git  --icons=always --no-time"
-#fi
+if command -v eza &> /dev/null; then
+	alias ls="eza --color=always --long --git  --icons=always --no-time"
+fi
 alias cl="clear"
