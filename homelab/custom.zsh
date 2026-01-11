81270766769eda7213d0a8da14fc16f2312e82f1
Steven Nkeneng <nkeneng.junior@yahoo.fr>
2026-01-11 10:52:09 +0100

    cleanup dotfiles custom config

--- homelab/custom.zsh
+++ homelab/custom.zsh
@@ -1,17 +1,3 @@
-
-# Zoxide setup
-eval "$(zoxide init zsh)"
-
-#alias cdl='zoxide query -l -s | less'
-
-if [[ ! "$PATH" == *$HOME/.local/bin* ]]; then
-	PATH="${PATH:+${PATH}:}$HOME/.local/bin"
-fi
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
 code-server() {
 	docker stop code-server
 	docker rm code-server
@@ -25,16 +11,4 @@ code-server() {
 		--network nosql \
 		--restart unless-stopped \
 		code-server
-}
-
-code() {
-	docker stop 42
-	docker rm 42
-	docker run -it \
-		--name 42 \
-		-e PUID=1002 \
-		-e PGID=1002 \
-		-v ${PWD}:/home/coder/code \
-		-v ~/.ssh:/home/coder/.ssh \
-		registry.stevenshomelab.com/42-env
 }
--- macos/custom.zsh
+++ macos/custom.zsh
@@ -1,86 +1,27 @@
 
-# Zoxide setup
-eval "$(zoxide init zsh)"
-eval "$(fzf --zsh)"
-
-set -o vi
-
-# alias cdl='zoxide query -l -s | less'
-#alias cd=z
-
-if [[ ! "$PATH" == *$HOME/.local/bin* ]]; then
-	PATH="${PATH:+${PATH}:}$HOME/.local/bin"
-fi
-
-# alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
-
-export OLLAMA_ORIGINS="app://obsidian.md*,chrome-extension://*"
-
-export PATH=$PATH:/Users/stevennkeneng/Library/Python/3.9/bin
-alias wm="/Users/stevennkeneng/Documents/Website/42-Berlin/libft-war-machine/grademe.sh"
-
-
-function wm_libft() {
-    wm $(find . -type f -name '*.c' | sed 's/\.c$//')
-}
-
-export BAT_THEME=TwoDark
-
-alias norminette="find . -type f -name '*.h' -o -name '*.c' ! -name 'main.c' ! -name 'test.c' -exec norminette {} +"
-
-alias port-forward="~/Documents/Bash_Scripts/port-forward.sh"
-alias port-delete="~/Documents/Bash_Scripts/del-port-forward.sh"
-alias create-repo="~/Documents/Bash_Scripts/create-repository.sh"
-
-#eval "$(gh copilot alias -- zsh)"
-
-
-# export USER="snkeneng"
-export USER="stevennkeneng"
-export MAIL="snkeneng@student.42berlin.de"
-
-export EDITOR=nvim
+alias cdl='zoxide query -l -s | less'
+alias cd=z
 alias obs="cd '/Users/stevennkeneng/Library/Mobile Documents/iCloud~md~obsidian/Documents/Coding journey' && nvim ."
 
-
-# nvim config switcher
-alias nvim-lazy="NVIM_APPNAME=lazy_nvim nvim"
-
-function nvims() {
-  items=("default" "lazy_nvim")
-  config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
-  if [[ -z $config ]]; then
-    echo "Nothing selected"
-    return 0
-  elif [[ $config == "default" ]]; then
-    config=""
-  fi
-  NVIM_APPNAME=$config nvim $@
-}
-
-bindkey -s ^a "nvims\n"
-
-alias bashly='docker run --rm -it --user $(id -u):$(id -g) --volume "$PWD:/app" dannyben/bashly'
-
-export ANDROID_HOME=/Users/stevennkeneng/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-
-42-code() {
-	docker stop 42
-	docker rm 42
-	docker run -it \
-		--name 42 \
-		-e PUID=1002 \
-		-e PGID=1002 \
-		-v ${PWD}:/home/coder/code \
-		-v ~/.ssh:/home/coder/.ssh \
-		registry.stevenshomelab.com/42-env
-}
+export OLLAMA_ORIGINS="app://obsidian.md*,chrome-extension://*"
+export BAT_THEME=TwoDark
+export USER="stevennkeneng"
+export EDITOR=nvim
 
 
+# # nvim config switcher for multiple configs
+# alias nvim-lazy="NVIM_APPNAME=lazy_nvim nvim"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+# function nvims() {
+#   items=("default" "lazy_nvim")
+#   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+#   if [[ -z $config ]]; then
+#     echo "Nothing selected"
+#     return 0
+#   elif [[ $config == "default" ]]; then
+#     config=""
+#   fi
+#   NVIM_APPNAME=$config nvim $@
+# }
 
+# bindkey -s ^a "nvims\n"
